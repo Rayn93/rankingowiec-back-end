356ccc9929307cdb91928379aca38308f45ec15f
Rob <robert.saternus@gmail.com>
2017-05-03 15:02:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+rankingowiec
+============
+
+A Symfony project created on April 29, 2017, 2:40 pm.
---
+++ src/RankingowiecBundle/RankingowiecBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RankingowiecBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RankingowiecBundle extends Bundle
+{
+}
